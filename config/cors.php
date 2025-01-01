0997404e97245682d449218f3f279646b00e3163
DGrabarz <Dawid.Grabarz@hemmersbach.com>
2025-01-01 17:09:00 +0100

    cors config updated

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173'],
+    'allowed_origins' => [env('DOMAIN_NAME', 'localhost')],
 
     'allowed_origins_patterns' => [],
 
